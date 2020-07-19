c3b90d6662441a6acdc76dbed6ab7440586310eb
Serdar <serdar2225@gmail.com>
2020-07-18 22:59:23 -0400

    first feature file and step definition

---
+++ src/test/java/stepDefinitions/loginStepDefinitions.java
@@ -0,0 +1,33 @@
+package stepDefinitions;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class loginStepDefinitions {
+
+    @Given("^User is on the Login Page$")
+    public void user_is_on_the_Login_Page() throws Throwable {
+
+    }
+
+    @When("^Title of Login Pages is Free CRM$")
+    public void title_of_Login_Pages_is_Free_CRM() throws Throwable {
+
+    }
+
+    @Then("^User enters username and password$")
+    public void user_enters_username_and_password() throws Throwable {
+
+    }
+
+    @Then("^User clicks on login button$")
+    public void user_clicks_on_login_button() throws Throwable {
+
+    }
+
+    @Then("^User is on home page$")
+    public void user_is_on_home_page() throws Throwable {
+
+    }
+}
---
+++ src/test/resources/Features/login.feature
@@ -0,0 +1,9 @@
+Feature: Free CRM Login Feature
+
+  Scenario: Free CRM Login Test Scenario
+
+    Given User is on the Login Page
+    When Title of Login Pages is Free CRM
+    Then User enters username and password
+    Then User clicks on login button
+    And User is on home page
